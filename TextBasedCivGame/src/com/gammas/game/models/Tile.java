33f9dbb0c2597871c1fa84101e1bf7dbcc15ef23
James Carter <James Carter@DESKTOP-3KP114E.Home>
2016-08-25 10:58:52 +0100

    Deleted old folder

--- TextBasedCivGame/src/com/gammas/game/models/Tile.java
+++
@@ -1,9 +0,0 @@
-package com.gammas.game.models;
-
-public class Tile extends GameObject {
-
-	public Tile(int _x, int _y, int _width, int _height, String imageFileName) {
-		super(_x, _y, _width, _height, imageFileName);
-	}
-
-}

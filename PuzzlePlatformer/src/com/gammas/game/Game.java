e6a5e4793fb43e3b5b581bd8abb45e906d9e0563
James Carter <James Carter@DESKTOP-3KP114E.Home>
2016-08-25 13:51:01 +0100

    Started basic implementation of Lua into the Game. This will be so GameObjects can have custom Abilities/Functions/Update Methods!
    MODABILITY UPDATE #2

--- PuzzlePlatformer/src/com/gammas/game/Game.java
+++ PuzzlePlatformer/src/com/gammas/game/Game.java
@@ -2,9 +2,17 @@ package com.gammas.game;
 
 import java.io.IOException;
 
+import org.keplerproject.luajava.LuaState;
+import org.keplerproject.luajava.LuaStateFactory;
+
 public class Game {
 
 	public static void main(String[] args) throws IOException {
+		LuaState L = LuaStateFactory.newLuaState();
+		L.openLibs();
+		
+		L.LdoFile("Assets/Lua/hello.lua");
+		
 		new Platformer();
 	}
 
